15b8f4eab10d3b96fca976eb87f128d087f68381
damak <akinlade.co@gmail.com>
2022-06-15 05:37:35 +0100

    layout and custom document

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Layout from "../components/ui/layout";
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Zozo</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
