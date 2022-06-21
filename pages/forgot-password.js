20294d951806bf95fcbf64af44ccd6f8d7ec93e3
damak <akinlade.co@gmail.com>
2022-06-21 08:23:48 +0100

    forgot password page

---
+++ pages/forgot-password.js
@@ -0,0 +1,32 @@
+import { ForgotPasswordBanner } from "../public/svg/images";
+import Link from "next/link";
+import Button from "../components/ui/button/button";
+import styles from "../styles/forgot-password.module.scss";
+
+export default function index() {
+  function handleRequest(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <div>
+          <ForgotPasswordBanner />
+        </div>
+        <form onSubmit={handleRequest}>
+          <h1>Forgot Password?</h1>
+          <p>Don&apos;t worry we will help you out!!</p>
+          <div className={styles["form-group"]}>
+            <label htmlFor="email">Email Address</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <Button>Request Password Change</Button>
+          <span>
+            Do you need help? <Link href="/">Contact Support</Link>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
+}
