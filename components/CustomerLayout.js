ead6e4fcab5a066b8169988bd357177477740d38
damak <akinlade.co@gmail.com>
2022-06-22 19:41:27 +0100

    top margin

--- components/CustomerLayout.js
+++ components/CustomerLayout.js
@@ -2,16 +2,16 @@ import Nav from "./Nav";
 import Footer from "./Footer";
 
 function CustomerLayout({ children }) {
-	return (
-		<>
-			<Nav />
-			<main style={{ backgroundColor: "#E5E5E5", marginTop:"100px" }} >
-				<div className="w-11/12 mx-auto  py-20">{children}</div>
-			</main>
+  return (
+    <>
+      <Nav />
+      <main style={{ marginTop: "16rem" }}>
+        <div className="w-11/12 mx-auto  py-20">{children}</div>
+      </main>
 
-			<Footer />
-		</>
-	);
+      <Footer />
+    </>
+  );
 }
 
 export default CustomerLayout;
