14055ebdd1549cc2cead64db9ee267dde2ecc1e0
damak <akinlade.co@gmail.com>
2022-06-26 18:05:23 +0100

    bread crumb component

---
+++ components/bread-crumb/bread-crumb.js
@@ -0,0 +1,29 @@
+import { RightArrow } from "../../public/svg/icons";
+import Link from "next/link";
+import styles from "./bread-crumb.module.scss";
+
+export default function BreadCrumb({ data }) {
+  return (
+    <ul className={styles.list}>
+      {data.map((crumb, index) => {
+        if (!crumb.url) {
+          return (
+            <li key={crumb.text}>
+              {crumb.text}
+              {index !== data.length - 1 && <span>{">"}</span>}
+            </li>
+          );
+        } else {
+          return (
+            <li key={crumb.text}>
+              <Link href={crumb.url}>
+                <a>{crumb.text}</a>
+              </Link>
+              {index !== data.length - 1 && <span>{">"}</span>}
+            </li>
+          );
+        }
+      })}
+    </ul>
+  );
+}
