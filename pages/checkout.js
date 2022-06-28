992e90186d64542fa2b9e243687f3b3a8b016325
Chiamaka Ugoji <mychiamaka@gmail.com>
2022-06-28 16:21:54 +0100

    Update checkout

--- pages/checkout.js
+++ pages/checkout.js
@@ -3,18 +3,40 @@ import CustomerLayout from "../components/CustomerLayout";
 import styles from "./../styles/cart.module.scss";
 import Image from "next/image";
 import email from "../assets/email.svg";
+import card from "../assets/card.svg";
 
 const Checkout = () => {
   return (
     <CustomerLayout>
-      <div className="checkout-wrapper bg-white">
+      <div className="checkout-wrapper bg-white p-20">
         <div className="flex">
-          <div>
+          <div className="">
             <h1>Payment Details</h1>
-            <p>Enter your personal details to complete your profile</p>
-            <div><span className="mr-5">email address</span>  
-                <Image src={email}/>
-                <input className={`${styles.email}`} type="email" placeholder="email address" />
+            <p className="mb-10">
+              Enter your personal details to complete your profile
+            </p>
+            <div className="mb-5 ">
+              <span className="mr-5">email address</span>
+              <span className={`${styles.email_icon}`}>
+                <Image src={email} />
+              </span>
+              <input
+                className={`${styles.email}`}
+                type="email"
+                placeholder="email address"
+              />
+            </div>
+            <div>
+              <span className="mr-5">Card Number</span>
+              <span className={`${styles.email_icon}`}>
+                <Image src={card} />
+              </span>
+              <input
+                className={`${styles.email}`}
+                type="email"
+                placeholder="email address"
+              />
+              <input type="date" placeholder="MM/YY"/>
             </div>
           </div>
         </div>
