341ab3f75a76de6fadd3ce50b493504b369bd732
damak <akinlade.co@gmail.com>
2022-07-07 15:16:47 +0100

    merchant dashboard

---
+++ pages/merchant/dashboard.js
@@ -0,0 +1,10 @@
+import MerchantSideBar from "../../components/merchant-sidebar";
+import styles from "../../styles/merchant-dashboard.module.scss";
+
+export default function Index() {
+  return (
+    <div>
+      <MerchantSideBar />
+    </div>
+  );
+}
