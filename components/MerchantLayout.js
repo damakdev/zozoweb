9aff5068eb812a038d386d5f10613980b04e07ba
Okorie Emmanuel <teymee17@gmail.com>
2022-07-08 20:42:23 +0100

    Merchant Account Profile

---
+++ components/MerchantLayout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MerchantSideBar from "./merchant-sidebar/merchant-sidebar";
+import styles from "../styles/merchant-profile.module.scss";
+
+function MerchantLayout({ children }) {
+	return (
+		<>
+			<MerchantSideBar />
+
+			<div className={styles.children}>{children}</div>
+		</>
+	);
+}
+
+export default MerchantLayout;
