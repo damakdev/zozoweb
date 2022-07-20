c4db29313999afe7704009bb880412fe93bf40c4
damak <akinlade.co@gmail.com>
2022-07-20 16:27:36 +0100

    installed store

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,19 @@
 import Head from "next/head";
 import Layout from "../components/layout";
+import { store } from "../store";
+import { Provider } from "react-redux";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>Zozo</title>
       </Head>
       <Component {...pageProps} />
       {/* <Layout>
       </Layout> */}
-    </>
+    </Provider>
   );
 }
 
