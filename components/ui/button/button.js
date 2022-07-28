1fa57881ead0bbffae97c84a5092f880ed87fa6f
damak <akinlade.co@gmail.com>
2022-07-28 18:34:38 +0100

    added type prop

--- components/ui/button/button.js
+++ components/ui/button/button.js
@@ -1,8 +1,8 @@
 import styles from "./button.module.scss";
 
-export default function Button({ children, onClick }) {
+export default function Button({ children, onClick, type }) {
   return (
-    <button onClick={onClick} className={styles.button}>
+    <button type={type} onClick={onClick} className={styles.button}>
       {children}
     </button>
   );
