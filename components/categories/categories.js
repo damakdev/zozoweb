9f2c097833b5c4063b9f5ff3e64ccd190ba5b2ad
damak <akinlade.co@gmail.com>
2022-07-30 08:41:55 +0100

    categories

--- components/categories/categories.js
+++ components/categories/categories.js
@@ -1,24 +1,12 @@
 import styles from "./categories.module.scss";
 
-export default function Categories() {
-  const categories = [
-    "babies & moms",
-    "desktops",
-    "audios & videos",
-    "kids clothing",
-    "accessories",
-    "bags",
-    "clothings",
-    "footwears",
-    "men",
-    "cars",
-  ];
+export default function Categories({ categories }) {
   return (
     <div className={styles.container}>
       <p>Categories </p>
       <ul>
-        {categories.map((item, index) => (
-          <li key={index}>{item}</li>
+        {categories?.map((category) => (
+          <li key={category.id}>{category.name}</li>
         ))}
       </ul>
     </div>
