3721b97a9407c5cfefe632253b3f736c7071b1d1
damak <akinlade.co@gmail.com>
2022-07-30 08:42:15 +0100

    dispatch icons

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { _getAllCategories } from "../../store/slices/categoriesSlice";
+import { _getApprovedBiddingEvents } from "../../store/slices/eventsSlice";
 import styles from "./layout.module.scss";
 
 export default function Layout({ children }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(_getApprovedBiddingEvents());
+    dispatch(_getAllCategories());
+  }, []);
   return <div className={styles.container}>{children}</div>;
 }
