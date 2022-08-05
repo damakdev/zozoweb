d7d7b6b584b6ae80566d429261a6dc272db9e65c
damak <akinlade.co@gmail.com>
2022-08-05 04:42:13 +0100

    auth guards

---
+++ components/authGuards/index.js
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+
+export function CustomerAuthGuard({ children }) {
+  const router = useRouter();
+  const { user } = useSelector((state) => state.auth.customer);
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/");
+    }
+  }, [user, router]);
+
+  if (!user) {
+  }
+
+  return <>{children}</>;
+}
+
+export function MerchantAuthGuard({ children }) {
+  const router = useRouter();
+  const { user } = useSelector((state) => state.auth.merchant);
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/merchant");
+    }
+  }, [user, router]);
+
+  if (!user) {
+  }
+
+  return <>{children}</>;
+}
+
+export function AdminAuthGuard({ children }) {
+  const router = useRouter();
+  const { user } = useSelector((state) => state.auth.admin);
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/");
+    }
+  }, [user, router]);
+
+  if (!user) {
+  }
+
+  return <>{children}</>;
+}
