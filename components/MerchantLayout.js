fc4e65dbcf8ceaadb7eae1fb2791dd70a8cd5c4a
damak <akinlade.co@gmail.com>
2022-08-06 09:07:54 +0100

    auth protect

--- components/MerchantLayout.js
+++ components/MerchantLayout.js
@@ -3,16 +3,15 @@ import MerchantSideBar from "./merchant-sidebar/merchant-sidebar";
 import styles from "../styles/admin/admin-layout.module.scss";
 
 function MerchantLayout({ children }) {
-	return (
-		<div className="bg-white" style={{backgroundColor:"#E5E5E5"}}>
-			<MerchantSideBar />
-		
-			<div className={styles.children}>
-				
-				
-				{children}</div>
-		</div>
-	);
+  return (
+    <div className="bg-white" style={{ backgroundColor: "#E5E5E5" }}>
+      <MerchantSideBar />
+
+      <div className={styles.children}>{children}</div>
+    </div>
+  );
 }
 
 export default MerchantLayout;
+
+MerchantLayout.requireMerchantAuth = true;
