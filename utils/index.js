427d93fae1d4c7828f883d453d4f1e4e669cb6e3
damak <akinlade.co@gmail.com>
2022-08-06 09:16:35 +0100

    date to locale string function

--- utils/index.js
+++ utils/index.js
@@ -17,6 +17,10 @@ export function convertUtc(utc) {
   // return localDate.toLocaleString("en-GB", { hour12: true });
 }
 
+export function toLocaleString(date) {
+  return new Date(date).toLocaleString();
+}
+
 export function formatNumber(number) {
   return number.toLocaleString("en-US", {
     minimumIntegerDigits: 2,
