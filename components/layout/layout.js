ffeba0baab6d563f25d2d24cbf5ca0ea5a091ec9
damak <akinlade.co@gmail.com>
2022-08-11 06:01:35 +0100

    get bidding events and categories on index load

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { _getAllCategories } from "../../store/slices/categoriesSlice";
-import { _getApprovedBiddingEvents, _getBiddingEventByStatus } from "../../store/slices/eventsSlice";
+import {
+  _getApprovedBiddingEvents,
+  _getBiddingEventByStatus,
+} from "../../store/slices/eventsSlice";
 import { _getCurrentUser } from "../../store/slices/userSlice";
 import styles from "./layout.module.scss";
 
@@ -9,9 +12,9 @@ export default function Layout({ children }) {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(_getApprovedBiddingEvents());
+    // dispatch(_getApprovedBiddingEvents());
     dispatch(_getBiddingEventByStatus());
-    dispatch(_getAllCategories());
+    // dispatch(_getAllCategories());
     // dispatch(_getCurrentUser());
   }, []);
   return <div className={styles.container}>{children}</div>;
