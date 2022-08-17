2bfbc0643c482e3c7af736ef29ab3ed9ca22af55
damak <akinlade.co@gmail.com>
2022-08-17 20:12:17 +0100

    added loader

--- components/categories/categories.js
+++ components/categories/categories.js
@@ -1,10 +1,12 @@
+import Loader from "../loader";
 import styles from "./categories.module.scss";
 
 export default function Categories({ categories }) {
   return (
     <div className={styles.container}>
-      <p>Categories </p>
+      <p>Categories</p>
       <ul>
+        {!categories && <Loader />}
         {categories?.map((category) => (
           <li key={category.id}>{category.name}</li>
         ))}
