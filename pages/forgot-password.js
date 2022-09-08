31e891d6b741f8f798929cf72557e931634182db
damak <akinlade.co@gmail.com>
2022-09-08 07:04:26 +0100

    responsiveness

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -1,9 +1,12 @@
 import { ForgotPasswordBanner } from "../public/svg/images";
+import useWindowDimension from "../hooks/useWindowDimension";
 import Link from "next/link";
+import Logo from "../components/logo";
 import Button from "../components/ui/button/";
 import styles from "../styles/forgot-password.module.scss";
 
 export default function Index() {
+  const { width } = useWindowDimension();
   function handleRequest(e) {
     e.preventDefault();
   }
@@ -12,9 +15,11 @@ export default function Index() {
     <div className={styles.container}>
       <div className={styles.main}>
         <div>
+          {width > 780 && <Logo />}
           <ForgotPasswordBanner />
         </div>
         <form onSubmit={handleRequest}>
+          {width <= 780 && <Logo variant="purple" />}
           <h1>Forgot Password?</h1>
           <p>Don&apos;t worry we will help you out!!</p>
           <div className={styles["form-group"]}>
